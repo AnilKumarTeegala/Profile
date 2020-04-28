829dc0b497b1a50ae3294c264f1049a22969cbd2
Anil Kumar Teegala <34655912+AnilKumarTeegala@users.noreply.github.com>
2020-04-28 19:31:07 +0530

    Update precache-manifest.f19f25722ed3d576e396427c0dccd138.js

--- precache-manifest.f19f25722ed3d576e396427c0dccd138.js
+++ precache-manifest.f19f25722ed3d576e396427c0dccd138.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "45bd74a4788b6a535548f53c58370a3e",
-    "url": "/React-Profile/index.html"
+    "url": "/ReactProfile/index.html"
   },
   {
     "revision": "dbb6aa9ae92f73fdb49c",
-    "url": "/React-Profile/static/css/main.8f317531.chunk.css"
+    "url": "/ReactProfile/static/css/main.8f317531.chunk.css"
   },
   {
     "revision": "c47925e5cb280e74e8bc",
-    "url": "/React-Profile/static/js/2.7d18beab.chunk.js"
+    "url": "/ReactProfile/static/js/2.7d18beab.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/React-Profile/static/js/2.7d18beab.chunk.js.LICENSE.txt"
+    "url": "/ReactProfile/static/js/2.7d18beab.chunk.js.LICENSE.txt"
   },
   {
     "revision": "dbb6aa9ae92f73fdb49c",
-    "url": "/React-Profile/static/js/main.8eb7a479.chunk.js"
+    "url": "/ReactProfile/static/js/main.8eb7a479.chunk.js"
   },
   {
     "revision": "0e61044f93d62ab91fe1",
-    "url": "/React-Profile/static/js/runtime-main.17900df0.js"
+    "url": "/ReactProfile/static/js/runtime-main.17900df0.js"
   }
 ]);
